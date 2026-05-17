7d4d07cca2e8c9a3be9e1386904ca610cc8f75e7
Akash222005 <akashnatarajan10@gmail.com>
2026-05-17 13:42:02 +0530

    All Category Crud Operations Are Working

---
+++ InvoiceCoreAPI/Contracts/ICategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using InvoiceCoreAPI.DTO;
+using InvoiceCoreAPI.Entities;
+using InvoiceCoreAPI.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+namespace InvoiceCoreAPI.Contracts;
+
+public interface ICategoryRepository
+{
+    Task<int> AddAsync(Category category);
+    Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(int id);
+    Task<bool> UpdateAsync(Category category);
+    Task<bool> DeleteAsync(int id);
+
+    Task<PagedResultDto<Category>> GetAllPagedAsync(
+
+    string? Code,
+
+    string? Name,
+
+    int pageNumber,
+
+    int pageSize);
+
+}
